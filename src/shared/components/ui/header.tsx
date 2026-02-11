0117edfddf3a649fed13b069d8a7211ce6ee7cd3
harinsd404 <harinsd168@gmail.com>
2026-02-11 18:20:07 +0900

    fix: 드롭다운 트리거 접근성 향상 및 css 선택자 문제 해결

--- src/shared/components/ui/header.tsx
+++ src/shared/components/ui/header.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 const Header = () => {
   return (
-    <header className="py-base fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-gray-300 bg-white px-[7.5rem]">
+    <header
+      data-app-header
+      className="py-base fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-gray-300 bg-white px-4 md:px-8 lg:px-[7.5rem]"
+    >
       <Link href={'/'}>
         <Image src="/Logo.svg" alt="소곤 로고" width={86} height={34} className="cursor-pointer" />
       </Link>
-
       <nav className="text-md gap-base flex font-sans font-semibold text-gray-800">
         <Link href={'/'} className="p-xs flex justify-center">
           대시보드
@@ -18,10 +20,10 @@ const Header = () => {
           마이페이지
         </Link>
       </nav>
-      <div className="gap-xs flex cursor-pointer items-center">
+      <button className="gap-xs flex cursor-pointer items-center">
         <p className="font-sans font-semibold text-gray-800">하린 님</p>
         <Image src="/icon/caret-down.svg" alt="드롭다운 표시자 아이콘" width={24} height={24} />
-      </div>
+      </button>
     </header>
   );
 };
