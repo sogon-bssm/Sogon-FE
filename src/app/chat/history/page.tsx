265297f4f69d9611dec9279fe5de0960a819901e
harinsd404 <harinsd168@gmail.com>
2026-02-11 22:56:30 +0900

    feat:: 폴더구조 일부 변경 및 쿼리스트링으로 새 채팅 구별

---
+++ src/app/chat/history/page.tsx
@@ -0,0 +1,4 @@
+const ChatHistory = () => {
+  return <h1>채팅보관함 페이지입니다</h1>;
+};
+export default ChatHistory;
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,12 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 const Chat = () => {
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get('session'); //session이 있으면 값 반환, 없으면 null
+
+  if (sessionId) return <h1>{sessionId}</h1>;
   return <h1>채팅 페이지입니다</h1>;
 };
 export default Chat;
