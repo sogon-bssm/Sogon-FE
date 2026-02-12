e43bd90bed53ea50cd8d30147b7d62608c747cf7
harinsd404 <harinsd168@gmail.com>
2026-02-12 14:50:56 +0900

    feat:: 날짜변환 유틸 함수 생성

---
+++ src/shared/utils/date.ts
@@ -0,0 +1,14 @@
+export const getTimeAgo = (date: string) => {
+  const diff = Date.now() - new Date(date).getTime();
+  const minutes = Math.floor(diff / 60000);
+  if (minutes < 60) return `${minutes}분 전`;
+
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) return `${hours}시간 전`;
+
+  const days = Math.floor(hours / 24);
+  if (days < 7) return `${days}일 전`;
+
+  const d = new Date(date);
+  return `${d.getFullYear()}년 ${d.getMonth() + 1}월 ${d.getDate()}일`;
+};
