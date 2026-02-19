f0908853772227deea27ad76058502b17e6f91f4
harinsd404 <harinsd168@gmail.com>
2026-02-19 17:07:29 +0900

    fix:: 유틸함수 예외처리

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -24,5 +24,6 @@ export const getTimeAgo = (date: string) => {
 
 export const formatDate = (date: string) => {
   const d = new Date(date);
+  if (isNaN(d.getTime())) return '';
   return `${d.getFullYear()}. ${String(d.getMonth() + 1).padStart(2, '0')}. ${String(d.getDate()).padStart(2, '0')}`;
 };
