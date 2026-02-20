03a153154855624ecaf93eb89ba40eaa60c355ad
시은 <dawndrizzle09@gmail.com>
2026-02-20 13:42:32 +0900

    feat:: 로그인 페이지 퍼블리싱

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,4 +1,23 @@
-const SignUp = () => {
-  return <h1>회원가입 페이지입니다</h1>;
-};
-export default SignUp;
+import SignupForm from '@/features/auth/components/SignupForm';
+
+export default function SignupPage() {
+  return (
+    <div className="flex h-screen w-screen justify-center overflow-hidden">
+      <div
+        className="flex shrink-0"
+        style={{
+          width: '1440px',
+          height: '100vh',
+        }}
+      >
+        <div className="m-lg flex w-[726px] items-center justify-center rounded-xl bg-gray-100">
+          <img src="/sogon-character.png" alt="소곤 캐릭터" />
+        </div>
+
+        <div className="flex flex-1 items-center justify-center">
+          <SignupForm />
+        </div>
+      </div>
+    </div>
+  );
+}
