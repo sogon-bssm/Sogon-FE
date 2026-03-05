58c45a5b434cc7c8f8777458fea88fba8b08192c
harinsd404 <harinsd168@gmail.com>
2026-03-05 10:41:33 +0900

    feat:: 색상 추가 및 로그아웃 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,6 @@
 import ReportActionCard from '@/features/mypage/components/reportActionCard';
 import SettingSection from '@/features/mypage/components/settingSection';
+import Image from 'next/image';
 
 const Mypage = () => {
   return (
@@ -9,11 +10,13 @@ const Mypage = () => {
         <p className="text-md font-sans font-bold text-gray-800">하린 님</p>
       </div>
       <ReportActionCard />
-      <div>
+      <div className="gap-base flex flex-col">
         <h2 className="font-sans text-lg font-semibold text-gray-800">개인 설정</h2>
-        <div>
-          <SettingSection title="담당 친구 바꾸기" description="고냥이" link="" />
-          <SettingSection title="담당 친구 이름 바꾸기" description="고냥이" link="" />
+        <SettingSection title="담당 친구 바꾸기" description="고냥이" link="" />
+        <SettingSection title="담당 친구 이름 바꾸기" description="고냥이" link="" />
+        <div className="gap-sm flex flex-row">
+          <Image src="/icon/log-out.svg" alt="로그아웃 아이콘" width={24} height={24} />
+          <p className="text-md text-sementic-red font-sans font-semibold">로그아웃</p>
         </div>
       </div>
     </div>
